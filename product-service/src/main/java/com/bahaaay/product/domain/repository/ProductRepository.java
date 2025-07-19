7b9b9cb5f7e0daf5ece1508334dc97d9e5e23b0e
BahaaAY <bahaa.abouyassine2@gmail.com>
2025-07-19 14:46:15 +0300

    created product domain repo

--- product-service/src/main/java/com/bahaaay/product/domain/repository/ProductRepository.java
+++ product-service/src/main/java/com/bahaaay/product/domain/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.bahaaay.product.domain.repository;
 
+import com.bahaaay.product.domain.entity.Product;
+
+import java.util.UUID;
+
 public interface ProductRepository {
+    Product save(Product product);
 }

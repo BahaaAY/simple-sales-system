e72f8d5fd64df07ee56917545e6dd30b094f6477
BahaaAY <bahaa.abouyassine2@gmail.com>
2025-07-19 15:21:14 +0300

    created ProductJpaEntity and its required liquibase migrations

---
+++ product-service/src/main/java/com/bahaaay/product/infrastructure/persistence/entity/product/ProductJpaEntity.java
@@ -0,0 +1,40 @@
+package com.bahaaay.product.infrastructure.persistence.entity.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "product")
+public class ProductJpaEntity {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+}
---
+++ product-service/src/main/java/com/bahaaay/product/infrastructure/persistence/mapper/product/ProductPersistenceMapper.java
@@ -0,0 +1,7 @@
+package com.bahaaay.product.infrastructure.persistence.mapper.product;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductPersistenceMapper {
+}
